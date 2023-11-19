2ed4ae2fc07eaad4b9fb3a00e6f66c94144cd3ff
javi <javi@Javis-MacBook-Pro-3.local>
2023-11-18 19:12:21 -0800

    Add FreeBeats

--- cutiehack2023/src/index.js
+++ cutiehack2023/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Title from './Title'
+import FreeBeats from './FreeBeats'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
     <Title/>
+    <FreeBeats/>
+
   </React.StrictMode>
 );
 
